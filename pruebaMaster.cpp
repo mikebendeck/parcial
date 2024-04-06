82658d09e41eed7433f48ffb3f1f324d0d145449
mikebendeck <100166618+mikebendeck@users.noreply.github.com>
2024-04-05 19:45:34 -0500

    Update pruebaMaster.cpp
    Agregados comandos, hice la prueba con esta ventana abierta y no me dejo hacer el pull: 
    
    Miguel@DESKTOP-EL7AF1D MINGW64 ~/Desktop/Clon/parcial (master)
    $ git pull origin master
    remote: Enumerating objects: 4, done.
    remote: Counting objects: 100% (4/4), done.
    remote: Compressing objects: 100% (3/3), done.
    remote: Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
    Unpacking objects: 100% (3/3), 1.07 KiB | 78.00 KiB/s, done.
    From https://github.com/mikebendeck/parcial
     * branch            master     -> FETCH_HEAD
       7b8d284..dc72cf7  master     -> origin/master
    fatal: refusing to merge unrelated histories
    
    Miguel@DESKTOP-EL7AF1D MINGW64 ~/Desktop/Clon/parcial (master)
    $ git pull origin master
    From https://github.com/mikebendeck/parcial
     * branch            master     -> FETCH_HEAD
    fatal: refusing to merge unrelated histories

--- pruebaMaster.cpp
+++ pruebaMaster.cpp
@@ -1,2 +1,6 @@
 //Es una prueba para realizar un pull a la rama master
 // no me clonó esta rama; solo la main
+//Comando para crear una rama 
+//$ git branch master
+//comanco para cambiar a una rama
+//$ git checkout master
