dfd28acae71bb31d35854ee6a02c0e48d3274ce4
Pblshman <duiberhernando@hotmail.com>
2024-04-08 21:10:37 -0500

    Mejorando

---
+++ OperacionesMatrices.h
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+ 
+int** rotarMatriz(int**, int);
+int imprimirMatrix(int**, int);
+int copiaMatriz(int**, int**, int);
+
+int** crearMatrix(int numDim)
+{
+    int **ptr;
+    ptr = new int*[numDim];
+    
+    for (int i = 0; i < numDim; i++) {
+        ptr[i] = new int[numDim];
+    }
+    
+    int contador = 0;
+    for (int fila = 0; fila < numDim; fila++) {
+        for (int col = 0; col < numDim; col++) {
+            if (fila != numDim / 2 || col != numDim / 2) {
+                ptr[fila][col] = ++contador;
+            } else {
+                ptr[fila][col] = 0;
+            }
+        }
+    }
+    
+    return ptr;
+
+}
+
+
+int** rotarMatriz(int **matrizOriginal, int orden) {
+    int** matrizRotada = new int*[orden];
+    for (int i = 0; i < orden; i++) {
+        matrizRotada[i] = new int[orden];
+    }
+
+    for (int fila = 0; fila < orden; fila++) {
+        for (int col = 0; col < orden; col++) {
+            matrizRotada[orden - 1 - col][fila] = matrizOriginal[fila][col];
+        }
+    }
+
+    return matrizRotada;
+}
+
+int imprimirMatrix(int **matriz1, int orden){
+    for (int fila=0; fila<orden; fila++){
+        for (int col=0; col<orden; col++){
+            if (matriz1[fila][col]<10) cout << " ";
+            if (matriz1[fila][col]<100) cout << " ";            
+            if (matriz1[fila][col]) cout << matriz1[fila][col] << " ";
+            else cout << "* ";
+        }
+        cout << "   ";
+        cout << "\n";
+        
+    }
+    return 0;
+}
+
+int** copiaMatriz(int** matrizOrigen, int orden) {
+    int** matrizDestino = new int*[orden];
+    for (int fila = 0; fila < orden; fila++) {
+        matrizDestino[fila] = new int[orden];
+        for (int col = 0; col < orden; col++) {
+            matrizDestino[fila][col] = matrizOrigen[fila][col];
+        }
+    }
+    return matrizDestino;
+}
+
+
