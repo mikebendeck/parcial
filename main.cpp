4920278cfc0e53558cfa8a3c7b0018b3ca961652
Pblshman <duiberhernando@hotmail.com>
2024-04-08 21:07:13 -0500

    Mejorando

--- main.cpp
+++ main.cpp
@@ -1,43 +1,54 @@
 #include <iostream>
-#include "matrices(2).h"
+#include "OperacionesMatrices.h"
 #include "CrearEstructura.h"
+#include "ReglaEstructura.h"
 
 int main(){ 
 
-    int dimen;
-    cout << "Ingrese el numero de dimension que desea ingresar";
-    cin >> dimen;
+    int *** estructura ;
 
-    while (dimen < 0 || dimen % 2 == 0) {
-        cout << "Ingreso una dimension no valida" << endl;
-        cout << "Ingrese el numero de dimension que desea ingresar: ";
-        cin >> dimen;
-    }
+    int lenNumeros;
 
-    
-    imprimirMatrix(crearMatrix(dimen), dimen);
-    imprimirMatrix(rotarMatriz(crearMatrix(dimen), dimen), dimen);
+    cout << "Ingrese el tamaño de las matrices que van a componer la estructura. Recuerde que tienen que ser mayor que uno e impares" << endl;
+    cin >> lenNumeros;
 
-    int numeros[] = {3, 5, 7};
-    int*** estructura = create3DArray(numeros);
+    int* dimensionMatrices = new int[lenNumeros]{};
 
-    for( int eq = 0; eq <= 2; eq++ ){
+    for (int i = 0; i < lenNumeros; i++) {
 
-        for( int i = 0; i < numeros[eq]; i++){
+        cout << "Ingrese el tamaño de la matrix en la posicion " << i + 1 << endl;
+        cin >> dimensionMatrices[i];
 
-            for( int j = 0; j < numeros[eq]; j++){
+        while( dimensionMatrices[i] < 1 || dimensionMatrices[i] % 2 == 0){
 
-
-                cout << estructura[eq][i][j] << " ";
-
-            }
-
-            cout << endl;
-
+            cout << "Ingreso un numero menor que uno o par. Ingrese el tamaño del elemento en posicion " << i +1 << " de nuevo" << endl;
+            cin >> dimensionMatrices[i];
 
         }
 
-        cout << endl;
-
     }
+
+    estructura = create3DArray( dimensionMatrices, lenNumeros );
+
+    int posicionx;
+    int posiciony;
+
+    cout << "Ingrese el valor de posicionx: ";
+    cin >> posicionx;
+
+    cout << "Ingrese el valor de posiciony: ";
+    cin >> posiciony;
+
+    int*** lv = listaDeValores(estructura, 3, dimensionMatrices, posicionx, posiciony );
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            for (int k = 0; k < 2; k++) {
+                cout << lv[i][j][k] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
 }
